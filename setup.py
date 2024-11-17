065e468a09a6477213962710649ec91c976df996
jdjulius <julio.david.velasquez@gmail.com>
2024-11-16 21:34:33 -0600

    feat(functions): new graph functions barr,box and hist

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="UtilsAnalytics",
+    version="0.1",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    description="Utilidades para análisis de datos",
+    author="Tu Nombre",
+    author_email="tuemail@example.com",
+    url="https://github.com/jdjulius/UtilsAnalytics",
+)
