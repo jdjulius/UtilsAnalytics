450a5fac45c955fac7b24873f4291087c03dc067
jdjulius <julio.david.velasquez@gmail.com>
2024-11-16 22:17:38 -0600

    feat(functions): new graph functions barr,box and hist

--- src/utils.py
+++ src/utils.py
@@ -2,51 +2,57 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def graphBarrPlot(df, varCualitativa):
 
-    variables_numericas = df.select_dtypes(include=['int64', 'float64'])
+    variables_numericas = df.select_dtypes(include=["int64", "float64"])
     numVariables = len(variables_numericas.columns)
 
-    fig, axes = plt.subplots(nrows=numVariables, ncols=1, figsize=(10, 6*numVariables))
+    fig, axes = plt.subplots(
+        nrows=numVariables, ncols=1, figsize=(10, 6 * numVariables)
+    )
 
     for i, variable in enumerate(variables_numericas.columns):
         sns.barplot(x=varCualitativa, y=variable, ax=axes[i], data=df)
-        axes[i].set_title(f'Gráfica de {variable}')
+        axes[i].set_title(f"Gráfica de {variable}")
         axes[i].set_xlabel(variable)
-        axes[i].set_ylabel('Frecuencia')
+        axes[i].set_ylabel("Frecuencia")
 
     plt.tight_layout()
     plt.show()
-    
-    
+
+
 def graphBoxPlot(df, varCualitativa):
 
-    variables_numericas = df.select_dtypes(include=['int64', 'float64'])
+    variables_numericas = df.select_dtypes(include=["int64", "float64"])
     numVariables = len(variables_numericas.columns)
 
-    fig, axes = plt.subplots(nrows=numVariables, ncols=1, figsize=(10, 6*numVariables))
+    fig, axes = plt.subplots(
+        nrows=numVariables, ncols=1, figsize=(10, 6 * numVariables)
+    )
 
     for i, variable in enumerate(variables_numericas.columns):
         sns.boxplot(x=varCualitativa, y=variable, ax=axes[i], data=df)
-        axes[i].set_title(f'Gráfica de {variable}')
+        axes[i].set_title(f"Gráfica de {variable}")
         axes[i].set_xlabel(variable)
-        axes[i].set_ylabel('Frecuencia')
+        axes[i].set_ylabel("Frecuencia")
 
     plt.tight_layout()
     plt.show()
-    
-    def graphHistDensity(df, varCualitativa):
-        
-        variables_numericas = df.select_dtypes(include=['int64', 'float64'])
-        numVariables = len(variables_numericas.columns)
 
-        fig, axes = plt.subplots(nrows=numVariables, ncols=1, figsize=(10, 6*numVariables))
+def graphHistDensity(df, varCualitativa):
+    variables_numericas = df.select_dtypes(include=["int64", "float64"])
+    numVariables = len(variables_numericas.columns)
 
-        for i, variable in enumerate(variables_numericas.columns):
-            sns.histplot(x=variable, hue=varCualitativa, data=df, kde=True, ax=axes[i])
-            axes[i].set_title(f'Gráfica de {variable}')
-            axes[i].set_xlabel(variable)
-            axes[i].set_ylabel('Frecuencia')
+    fig, axes = plt.subplots(
+        nrows=numVariables, ncols=1, figsize=(10, 6 * numVariables)
+    )
 
-        plt.tight_layout()
-        plt.show()
+    for i, variable in enumerate(variables_numericas.columns):
+        sns.histplot(x=variable, hue=varCualitativa, data=df, kde=True, ax=axes[i])
+        axes[i].set_title(f"Gráfica de {variable}")
+        axes[i].set_xlabel(variable)
+        axes[i].set_ylabel("Frecuencia")
+
+    plt.tight_layout()
+    plt.show()
